a16e0147444d320d57e1e832ae55d5e6e9019e36
RS5Hack_ARIF <146631213+mdarif76769@users.noreply.github.com>
2026-05-27 10:02:46 +0600

    Update main.py
    Signed-off-by: RS5Hack_ARIF <146631213+mdarif76769@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
-TOKEN = "YOUR_BOT_TOKEN_HERE"  # আপনার বটের টোকেন দিন
+TOKEN = "8890338622:AAE2DhsEq6lkc86tFEca76YP22pCfoPkiAQ"  # আপনার বটের টোকেন দিন
 bot = telebot.TeleBot(TOKEN)
 
 # আপনার লাইভ ওয়েব অ্যাপ লিঙ্ক (গিটহাব পেজ লিঙ্ক)
@@ -9,7 +9,7 @@ MINI_APP_URL = "https://mdarif76769.github.io/mdarif76769/"
 
 def get_reply_keyboard():
     markup = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    btn_search = KeyboardButton("🔍 Search Apps", web_app=WebAppInfo(url=MINI_APP_URL))
+    btn_search = KeyboardButton("🔍 Search Apps", web_app=WebAppInfo(url=https://mdarif76769.github.io/mdarif76769/))
     btn_all_app = KeyboardButton("📱 All Apps")
     markup.add(btn_search, btn_all_app)
     return markup
